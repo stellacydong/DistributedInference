091083f614edd37b24dfb133e0c5177a52de4714
Stella Dong <stellacydong@gmail.com>
2024-04-29 22:36:55 -0700

    Create gpt2.sh

---
+++ gpt2.sh
@@ -0,0 +1,9 @@
+export NCCL_P2P_DISABLE="1"
+export NCCL_IB_DISABLE="1"
+
+# Set the correct number of processes and machines
+accelerate launch \
+  --num_processes 1 \
+  --num_machines 1 \
+  --mixed_precision "no" \
+  PytorchDistributedParallel/gpt2.py
