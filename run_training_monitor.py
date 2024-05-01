7b27c7e4a96dd58fe0d1f8a45bee2120c6787a7c
Stella Dong <stellacydong@gmail.com>
2024-05-01 02:03:51 -0700

    Update run_training_monitor.py

--- run_training_monitor.py
+++ run_training_monitor.py
@@ -16,7 +16,7 @@ from hivemind.optim.state_averager import TrainingStateAverager
 from hivemind.utils.logging import get_logger, use_hivemind_log_handler
 from hivemind.utils.networking import log_visible_maddrs
 
-import utils
+import DistributedInference/utils
 from arguments import AveragerArguments, BaseTrainingArguments, OptimizerArguments
 
 use_hivemind_log_handler("in_root_logger")
