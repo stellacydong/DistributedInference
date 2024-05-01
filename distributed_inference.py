3840caaf1c3b183837132881cca529c094db82b8
Stella Dong <stellacydong@gmail.com>
2024-04-30 18:43:21 -0700

    Update distributed_inference.py

--- distributed_inference.py
+++ distributed_inference.py
@@ -1,3 +1,5 @@
+# part 0 
+
 from accelerate import notebook_launcher
 
 def test():
@@ -6,3 +8,22 @@ def test():
     print(accelerator.distributed_type)
 
 notebook_launcher(test, num_processes=2)  
+
+# part 1 
+# simplest example: create strings on each GPU and collect them using gather_object()
+# change num_processes to the number of GPUs in your system
+
+from accelerate import notebook_launcher
+
+def hello_world():
+    from accelerate import Accelerator
+    from accelerate.utils import gather_object
+
+    accelerator = Accelerator()
+
+    message= [f"Hello this is GPU {accelerator.process_index}"]
+    messages=gather_object(message)
+
+    accelerator.print(messages)
+
+notebook_launcher(hello_world, num_processes=5)  
